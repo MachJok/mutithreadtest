5da2002e1b9445c24d90470bb047681ea658ab8c
MachJok <mikevivaldi@gmail.com>
2021-05-21 15:17:04 -0700

    cmake update

---
+++ CMakeFiles/libdiesel.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/libdiesel.dir/plugin.cpp.o"
+  "libdiesel.xpl"
+  "libdiesel.xpl.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/libdiesel.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
